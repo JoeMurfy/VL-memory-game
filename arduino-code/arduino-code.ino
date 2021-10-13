d72c1016795153e1c4691ce91832ec712b0e281b
JoeMurfy <joemurphybusiness@gmail.com>
2021-10-13 20:40:40 +0100

    A start on arduino code.
    Foundation for puzzle. Effectively a combination lock with 2 LEDs. Set up for expansion.

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,54 +1,45 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+
+
 #define DEBUG
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for 20 chars/4 line display.
 
-
+// Constants
 // How many buttons we use. Currently 5: Red, Blue, Green, Yellow and White
 const byte NUM_OF_BUTTONS = 5;
-
+// What pins the buttons go to on arduino. 
+const byte BUTTON_PINS[NUM_OF_BUTTONS] = {6, 5, 4, 3, 2};
 // Length of correct code. Currently 5 as only 5 lights on wall.
 const byte CODE_LENGTH = 5;
-
-// What pins the LEDs and buttons go to on arduino. 
-const byte LED_PINS[CODE_LENGTH] = {12, 11, 10, 9, 8};
-const byte BUTTON_PINS[NUM_OF_BUTTONS] = {6, 5, 4, 3, 40};
-
 // SET CORRECT COLOUR ORDER HERE.
 const byte CORRECT_CODE[CODE_LENGTH] = {0, 1, 2, 3, 4};
+// What pins the LEDs go to on arduino. 
+const byte LED_PINS[CODE_LENGTH] = {9, 8};
 
-
+// GLOBALS
 // Using internal pull-up resistors on buttons therefore when they are not being pressed they should read HIGH.
 bool lastButtonState[] = {HIGH, HIGH, HIGH, HIGH};
 // What position the code is actually on.
 int codePosition = 0;
 
-// Debouncing work around.
+// Debouncing work around. Increase delay if still bouncing.
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
 
 
 void setup() {
 
-  // Initialise the LCD and prints prompt.
-  lcd.begin();
-  lcd.backlight();
-  lcd.print("INPUT COLOUR ORDER");
-
-
   // Initialise all buttons connected to pins as pull ups (pulls button pin signal to high rather than floating and causing problems).
   for(int i = 0; i < NUM_OF_BUTTONS; i++){
     pinMode(BUTTON_PINS[i], INPUT_PULLUP);
   }
   
-  // Initialise the LED pins that show progress through the sequence 
+  // Initialise LEDs as outputs.
   for(int i = 0; i < CODE_LENGTH; i++){
     pinMode(LED_PINS[i], OUTPUT);
   }
 
   #ifdef DEBUG
     Serial.begin(9600);
-    Serial.println(F("Debug started"));
+    Serial.println(F("Debug begin."));
   #endif
 }
 
@@ -65,46 +56,48 @@ void loop() {
       // Debouncing.
       if(buttonState != lastButtonState[i]) {
         lastDebounceTime = millis();    
-      }        
+      }
+        
       if(buttonState == LOW && lastButtonState[i] == HIGH) {
       
         // Checking current button against correct code.
         if(CORRECT_CODE[codePosition] == i) {
           codePosition++;
-        
+          
           #ifdef DEBUG
             Serial.print(F("Correct input! Onto step #"));
             Serial.println(codePosition);
           #endif
         }
-        // Incorrect so resets code.
+        // Incorrect so resets code and turns flashes red LED.
         else {
           codePosition = 0;
-          Serial.println(F("Incorrect input! Back to the beginning!"));
+          digitalWrite(LED_PINS[0], HIGH);
+          delay(10);
+          digitalWrite(LED_PINS[0], LOW);
+          Serial.println(F("Incorrect"));
+        }
       }
-     
+      
       // Debouncing
       lastButtonState[i] = buttonState;
     }
-   
   }
 
-  // Check if code entered is correct.
+  // Check if colour order entered is correct then calls reward function.
   if(codePosition == CODE_LENGTH){
     reward();
-    Serial.println("correctcodee");
   }
- }
+
 }
 
-// Function for when they enter the correct colour code.
+// Function for when colour order entered correctly.
 void reward(){
-  // Show reward code and congratulations on screen.
-  lcd.clear();
-  lcd.home();
-  lcd.print("COLOUR ORDER CORRECT");
-  lcd.setCursor(0, 1);
-  lcd.setCursor(8, 2);
-  lcd.print("6598");
-  Serial.println("Correctcode");
+
+  #ifdef DEBUG
+    Serial.println(F("Puzzle Solved!"));
+  #endif 
+  
+  // Turn on Green LED
+  digitalWrite(LED_PINS[1], HIGH);
 }
